9630f717903ef2486556f4cdd04fe88054583de3
Aaron Kelley <aaronfkelley@gmail.com>
2022-10-05 23:45:50 -0600

    lsp-mode and lsp-pyright

--- init.el
+++ init.el
@@ -122,11 +122,26 @@
   :bind (("C-x C-j" . dired-jump))
   :custom ((dired-listing-switches "-agho --group-directories-first")))
 
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-enable-snippet nil)
+  :hook (
+	 (python-mode . lsp-deferred)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deferred))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-magit magit projectile helpful evil-collection evil ivy-rich counsel which-key rainbow-delimiters ace-window command-log-mode use-package)))
+   '(lsp-pyright lsp-mode evil-magit magit projectile helpful evil-collection evil ivy-rich counsel which-key rainbow-delimiters ace-window command-log-mode use-package)))
 
